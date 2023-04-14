faf1e289543be4c1baba8306387e7e5a2c5a281f
Fabio Abreu <fabiofiqueli@hotmail.com>
2023-04-14 09:20:09 +0100

    Criada a interface atividade2 para amostrar a mensagem recebida

---
+++ app/src/main/java/com/example/aplicacao1404/MainActivity2_MostrarMensagem.kt
@@ -0,0 +1,11 @@
+package com.example.aplicacao1404
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2_MostrarMensagem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity2_mostrar_mensagem)
+    }
+}
